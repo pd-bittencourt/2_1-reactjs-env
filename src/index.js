354f02ba3f5841d8e4e14a5681783c2e52b8a65f
Pedro <pedrodmbc@gmail.com>
2019-05-15 10:31:14 -0300

    Proptypes & defaultProps & children test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-alert("hello");
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { render } from "react-dom";
+
+class Button extends Component {
+  render() {
+    return (
+      <a href="#" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+Button.defaultProps = {
+  children: "Salvar"
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string
+};
+
+class App extends Component {
+  handleClick() {
+    alert("clicked");
+  }
+  render() {
+    return (
+      <>
+        <h1>Test</h1>
+        <Button
+          onClick={() => {
+            alert("btn1");
+          }}
+        />
+        <Button onClick={this.handleClick}>Enviar</Button>
+      </>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
